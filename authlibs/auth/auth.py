09f8ec53f78cd7707e52287ba47e39326bc08e2e
Brad Goodman <brad@bradgoodman.com>
2019-02-02 00:04:48 -0500

    Auth page now really authorizes Added colors/levels to flashed messages

--- authlibs/auth/auth.py
+++ authlibs/auth/auth.py
@@ -5,7 +5,7 @@ from flask import Flask, request, session, g, redirect, url_for, \
 	abort, render_template, flash, Response,Blueprint
 #from flask.ext.login import LoginManager, UserMixin, login_required,  current_user, login_user, logout_user
 from flask_login import LoginManager, UserMixin, login_required,  current_user, login_user, logout_user
-from ..db_models import Member, db, Resource
+from ..db_models import Member, db, Resource, AccessByMember
 from functools import wraps
 import json
 #from .. import requireauth as requireauth
@@ -38,21 +38,35 @@ def authorize():
       i=0
       while "memberid_"+str(i) in request.form:
         temp = request.form['memberid_'+str(i)]
+        print "GOT MEMBER_ID",temp
         members.append(temp)
         i+=1
       i=0
       while "resource_"+str(i) in request.form:
         temp = request.form['resource_'+str(i)]
-        resources.append(temp)
+        resources.append(Resource.query.filter(Resource.name == temp).one())
         i+=1
-      #print "MEMBERS",members
-      #print "RESOURCES",resources
       if len(members) == 0:
         others['error']="No members specified to authorize"
       elif len(resources) == 0:
         others['error']="No resources specified to authorize"
       else:
-          others['message']="Authorized "+" ".join(members)+" on "+" ".join(resources)
+        error=False
+        for m in members:
+            for r in resources:
+                mem=AccessByMember.query.join(Member,((Member.id==AccessByMember.member_id) & (Member.member==m) & (AccessByMember.resource_id == r.id))).one_or_none()
+                if mem:
+                    flash("%s already has access to %s." % (m,r.name),"warning")
+                else:
+                    (level,levelText)=authutil.getResourcePrivs(resource=r)
+                    if (level <= 0): 
+                        flash("You don't have privileges to grant access on %s" % r,"danger")
+                    else:
+                        flash("%s granted access to %s" % (m,r.name),"success")
+                        db.session.add(AccessByMember(member_id=Member.query.filter(Member.member == m).with_entities(Member.id),resource_id=r.id,level=0))
+
+        db.session.commit()
+        #others['message']="Authorized "+" ".join(members)+" on "+" ".join([x.name for x in resources])
     if 'search' in request.form and (request.form['search'] != ""):
       searchstr = authutil._safestr(request.form['search'])
       sstr = "%"+searchstr+"%"
@@ -62,6 +76,7 @@ def authorize():
 
     resources = Resource.query.all()
     res=[]
+
     for r in resources:
         (level,levelText)=authutil.getResourcePrivs(resource=r)
         res.append({'resource':r,'level':level,'levelText':levelText})
