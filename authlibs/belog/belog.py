4beddb7414a5ca74268775f99da1fb9645d6626d
Brad Goodman <brad@bradgoodman.com>
2019-02-07 22:50:27 -0500

    Backend log CSV download

--- authlibs/belog/belog.py
+++ authlibs/belog/belog.py
@@ -52,11 +52,20 @@ modules = ["authserver",
 @login_required
 def belog():
 		logs=[]
+		format="html"
 		meta={}
 		limit=200
 		offset=1
 		module_filters=[]
 		level_filters=[]
+
+		if 'format' in request.values:
+			format = request.values['format']
+			offset=0
+			limit=0
+		else:
+						if 'limit' in request.values: limit=int(request.values['limit'])
+						if 'offset' in request.values: offset=int(request.values['offset'])
 
 		for x in request.values:
 			if x.startswith("input_level_"):
@@ -65,8 +74,6 @@ def belog():
 				module_filters.append(x.replace("input_module_",""))
 		
 
-		if 'limit' in request.values: limit=int(request.values['limit'])
-		if 'offset' in request.values: offset=int(request.values['offset'])
 		if (offset <= 0): offset=1
 		fd = open(GLOBAL_LOGGER_LOGFILE)
 
@@ -80,41 +87,70 @@ def belog():
 		i=1
 		count=0
 		meta['displayoffset']=offset
-		for l in reversed(fd.readlines()):
-			when=l[0:19]
-			sp=l[24:].strip().split(":",4)
-			if (i>=offset):
-				color ="#fff"
-				if sp[0]=="WARNING": color="#ffd"
-				if sp[0]=="ERROR": color="#fdd"
-				if sp[0]=="DEBUG": color="#cff"
-				if sp[0]=="CRITICAL": color="#f00"
-				dateok = True
-				dt = datetime.datetime.strptime(when[0:10],"%Y-%m-%d")
-				if startdate and dt < startdate: dateok = False
-				if enddate and dt > enddate: dateok = False
-				if sp[0] not in level_filters and sp[1] not in module_filters and dateok:
-					if (limit == 0) or (count < limit):
-						logs.append({'when':when,'level':sp[0],'what':sp[1],'color':color,'message':sp[2].strip()})
-						meta['lastoffset']=i
-						count +=1
-			meta['count']=i
-			i+=1
-			#if (count >= limit): break
+
+		def do_generate():
+						for l in reversed(fd.readlines()):
+							when=l[0:19]
+							sp=l[24:].strip().split(":",4)
+							dateok = True
+							try:
+											dt = datetime.datetime.strptime(when[0:10],"%Y-%m-%d")
+											if startdate and dt < startdate: dateok = False
+											if enddate and dt > enddate: dateok = False
+							except:
+								pass
+							if sp[0] not in level_filters and sp[1] not in module_filters and dateok:
+									yield "\"%s\",\"%s\",\"%s\",\"%s\"\n" %(when,sp[0],sp[1],sp[2])
+
+		if format == "html":
+						for l in reversed(fd.readlines()):
+							when=l[0:19]
+							sp=l[24:].strip().split(":",4)
+							if (len( sp) < 3): continue
+							if (i>=offset):
+								color ="#fff"
+								if sp[0]=="WARNING": color="#ffd"
+								if sp[0]=="ERROR": color="#fdd"
+								if sp[0]=="DEBUG": color="#cff"
+								if sp[0]=="CRITICAL": color="#f00"
+								dateok = True
+								try:
+												dt = datetime.datetime.strptime(when[0:10],"%Y-%m-%d")
+												if startdate and dt < startdate: dateok = False
+												if enddate and dt > enddate: dateok = False
+								except:
+									pass
+					
+								if sp[0] not in level_filters and sp[1] not in module_filters and dateok:
+									if (limit == 0) or (count < limit):
+										logs.append({'when':when,'level':sp[0],'what':sp[1],'color':color,'message':sp[2].strip()})
+										meta['lastoffset']=i
+										count +=1
+							meta['count']=i
+							i+=1
+		elif format == "csv":
+				resp=Response(do_generate(),mimetype='text/csv')
+				resp.headers['Content-Disposition']='attachment'
+				resp.headers['filename']='log.csv'
+				return resp
+		else:
+				flash ("Invalid format requested","danger")
+				return redirect_url(request.url);
+
 		baseurl=url_for("belog.belog")
 		filterstr = ("&".join([("%s=1" % x) for x in (module_filters+level_filters)]))
 		if filterstr: filterstr = "&"+filterstr
 		if limit != 0:
 			meta['first'] = baseurl+("?limit=%s&offset=%s" % (limit,"1"))+filterstr
 			meta['last'] = baseurl+("?limit=%s&offset=%s" % (limit, meta['count']-limit))+filterstr
-			print "%s < %s" % (offset+limit , i)
 			if ((offset+limit) < i) and 'lastoffset' in meta:
 				meta['next'] = baseurl+("?limit=%s&offset=%s" % (limit, meta['lastoffset']+1))+filterstr
 			if (offset > 1):
 				meta['prev'] = baseurl+("?limit=%s&offset=%s" % (limit, offset-limit))+filterstr
+		meta['csvurl'] = baseurl+"?format=csv"+filterstr
 		
-		print meta
 		return render_template('belog.html',logs=logs,meta=meta,modules=sorted(modules))
+
 
 
 def register_pages(app):
